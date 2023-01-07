f14ab5261c17d63d7026595e1b7ef4055ddac60a
Niels <111445696+NielsNL1@users.noreply.github.com>
2023-01-07 20:46:12 +0100

    Delete background.js

--- background.js
+++
@@ -1,5 +0,0 @@
-const selectElement = document.querySelector('#background-select');
-
-selectElement.addEventListener('change', (event) => {
-    document.body.style.backgroundImage = `url(${event.target.value})`;
-});
